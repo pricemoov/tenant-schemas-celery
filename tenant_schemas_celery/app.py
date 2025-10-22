9d4edd25a3ab05b71253bc91d61e5b4c66bc7fe5
Étienne Lebrun <etienne.lebrun@pricemoov.com>
2025-10-22 11:12:18 +0200

    fix: unbound schema_name
    also:
     - drops some celery<4 paths
     - drops some cruft

--- tenant_schemas_celery/app.py
+++ tenant_schemas_celery/app.py
@@ -1,52 +1,43 @@
 from __future__ import absolute_import
 
-try:
-    import celery
-    from celery import Celery
-except ImportError:
-    raise ImportError("celery is required to use tenant_schemas_celery")
+from celery import Celery
 
-from django.db import connections
 from django_pgschemas import get_current_schema, activate, SchemaDescriptor
 from celery.signals import task_prerun, task_postrun
 
 
-def get_schema_name_from_task(task, kwargs):
-    if celery.VERSION[0] < 4:
-        # Pop it from the kwargs since tasks don't except the additional kwarg.
-        # This change is transparent to the system.
-        return kwargs.pop("_schema_name", None)
-
+def get_schema_name_from_task(task):
     # In some cases (like Redis broker) headers are merged with `task.request`.
     task_headers = task.request.headers or task.request
     return task_headers.get("_schema_name")
 
 
-def switch_schema(task, kwargs, **kw):
-    """ Switches schema of the task, before it has been run. """
+def switch_schema(task, **kw):
+    """Switches schema of the task, before it has been run."""
 
     old_schema = get_current_schema().schema_name
-
     setattr(task, "_old_schema", old_schema)
-    schema = get_schema_name_from_task(task, kwargs)
+
+    schema = get_schema_name_from_task(task)
+
     if old_schema == schema:
         # If the schema has not changed, don't do anything.
         return
     activate(SchemaDescriptor.create(schema))
 
 
-
 def restore_schema(task, **kwargs):
-    """ Switches the schema back to the one from before running the task. """
+    """Switches the schema back to the one from before running the task."""
 
-    if hasattr(task, "_old_schema"):
-        schema_name = task._old_schema
+    try:
+        schema_name = getattr(task, "_old_schema")
+    except AttributeError:
+        return
     current_schema = get_current_schema()
     if current_schema.schema_name == schema_name:
         # If the schema has not changed, don't do anything.
         return
     activate(SchemaDescriptor.create(schema_name))
-
 
 
 task_prerun.connect(
@@ -59,8 +50,8 @@ task_postrun.connect(
 
 
 class CeleryApp(Celery):
-    registry_cls = 'tenant_schemas_celery.registry:TenantTaskRegistry'
-    
+    registry_cls = "tenant_schemas_celery.registry:TenantTaskRegistry"
+
     def create_task_cls(self):
         return self.subclass_with_self(
             "tenant_schemas_celery.task:TenantTask",
@@ -69,25 +60,18 @@ class CeleryApp(Celery):
             attribute="_app",
         )
 
-    def _update_headers(self, kw):
-        kw["headers"] = kw.get("headers") or {}
-        self._add_current_schema(kw["headers"])
+    def _update_headers(self, options: dict):
+        headers = options.setdefault("headers", {})
 
-    def _add_current_schema(self, kwds):
+        schema_name = headers.get("_schema_name", get_current_schema().schema_name)
+        headers["_schema_name"] = schema_name
 
-        schema_name = kwds.get("_schema_name", get_current_schema().schema_name)
-        kwds["_schema_name"] = schema_name
-        
-        if "headers" not in kwds:
-            kwds["headers"] = {}
-        kwds["headers"]["_schema_name"] = schema_name
+        if "headers" not in headers:
+            headers["headers"] = {}
+        headers["headers"]["_schema_name"] = schema_name
 
     def send_task(self, name, args=None, kwargs=None, **options):
-        if celery.VERSION[0] < 4:
-            kwargs = kwargs or {}
-            self._add_current_schema(kwargs)
-
-        else:
-            # Celery 4.0 introduced strong typing and the `headers` meta dict.
-            self._update_headers(options)
-        return super(CeleryApp, self).send_task(name, args=args, kwargs=kwargs, **options)
+        self._update_headers(options)
+        return super(CeleryApp, self).send_task(
+            name, args=args, kwargs=kwargs, **options
+        )
--- tenant_schemas_celery/task.py
+++ tenant_schemas_celery/task.py
@@ -1,9 +1,9 @@
 import celery
 from celery.app.task import Task
-from django.db import connection
 from django_pgschemas import get_current_schema
 
 from tenant_schemas_celery.cache import SimpleCache
+
 _shared_storage = {}
 
 
@@ -13,8 +13,8 @@ class SharedTenantCache(SimpleCache):
 
 
 class TenantTask(Task):
-    """ Custom Task class that injects db schema currently used to the task's
-        keywords so that the worker can use the same schema.
+    """Custom Task class that injects db schema currently used to the task's
+    keywords so that the worker can use the same schema.
     """
 
     abstract = True
@@ -25,24 +25,10 @@ class TenantTask(Task):
     def tenant_cache(cls):
         return SharedTenantCache()
 
-    @classmethod
-    def get_tenant_for_schema(cls, schema_name):
-        from .compat import get_tenant_model
-        return get_tenant_model().objects.get(schema_name=schema_name)
-
     def _update_headers(self, kw):
-        kw["headers"] = kw.get("headers") or {}
-        self._add_current_schema(kw["headers"])
-
-    def _add_current_schema(self, kwds):
-        kwds["_schema_name"] = kwds.get("_schema_name", get_current_schema().schema_name)
+        headers = kw.setdefault("headers", {})
+        headers.setdefault("_schema_name", get_current_schema().schema_name)
 
     def apply(self, args=None, kwargs=None, *arg, **kw):
-        if celery.VERSION[0] < 4:
-            kwargs = kwargs or {}
-            self._add_current_schema(kwargs)
-
-        else:
-            # Celery 4.0 introduced strong typing and the `headers` meta dict.
-            self._update_headers(kw)
+        self._update_headers(kw)
         return super(TenantTask, self).apply(args, kwargs, *arg, **kw)
